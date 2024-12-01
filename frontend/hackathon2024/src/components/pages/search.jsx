ca5c5110e9becf4306bd5789c749960d46cc2408
Jakub <kubusiasty@interia.pl>
2024-12-01 08:42:17 +0100

    Filtrowanie alfabetyczne wyników

--- frontend/hackathon2024/src/components/pages/search.jsx
+++ frontend/hackathon2024/src/components/pages/search.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../header/header";
 import Navigation from "../navigation/navigation";
 import Footer from "../footer/footer";
 import HobbySearcher from "../hobbySearcher/hobbySearcher";
+import Filter from "../filter/filter";
 
-function Search(){
-    return(
-        <div className='mainContainer'>
-            <Header/>
-            <Navigation/>
-            <HobbySearcher address='/hobby_template.json'/>
-            <Footer/>
+function Search() {
+    const [isAlphabetical, setIsAlphabetical] = useState(false);
+    const [isReverseAlphabetical, setIsReverseAlphabetical] = useState(false);
+
+    const handleClickReset = () => {
+        setIsAlphabetical(false);
+        setIsReverseAlphabetical(false);
+    };
+
+    const handleClickAlphabetical = () => {
+        setIsAlphabetical(true);
+        setIsReverseAlphabetical(false);
+    };
+
+    const handleClickReverseAlphabetical = () => {
+        setIsAlphabetical(false);
+        setIsReverseAlphabetical(true);
+    };
+
+    return (
+        <div className="mainContainer">
+            <Header />
+            <Navigation />
+            <Filter
+                onReset={handleClickReset}
+                onAlphabetical={handleClickAlphabetical}
+                onReverseAlphabetical={handleClickReverseAlphabetical}
+            />
+            <HobbySearcher
+                address="/hobby_template.json"
+                isAlphabetical={isAlphabetical}
+                isReverseAlphabetical={isReverseAlphabetical}
+            />
+            <Footer />
         </div>
     );
 }
