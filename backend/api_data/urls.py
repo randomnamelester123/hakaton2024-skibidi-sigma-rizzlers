1859d2219d78fa26a89a8ab90f0766b764b79fa1
Maks588 <maksymilian.tab@gmail.com>
2024-12-01 02:36:01 +0100

    Hackaton

---
+++ backend/api_data/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ArticleList, ArticleDetail
+
+urlpatterns = [
+    path('', ArticleList.as_view(), name='article-list'),
+    path('<int:pk>/', ArticleDetail.as_view(), name='article-detail'),
+]
