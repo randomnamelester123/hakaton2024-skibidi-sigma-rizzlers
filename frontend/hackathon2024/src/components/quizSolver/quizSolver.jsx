76c5da03b603edb017b0850c5fb00c5f48899809
Jakub <kubusiasty@interia.pl>
2024-12-01 02:27:21 +0100

    Odczytywanie pytań z quizu

--- frontend/hackathon2024/src/components/quizSolver/quizSolver.jsx
+++ frontend/hackathon2024/src/components/quizSolver/quizSolver.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import {Component, React, useEffect} from "react";
 import './quizSolver.css';
+import axios from "axios";
 
-function QuizSolver(props){
-    return(
-        <div className='container'>
-            <h1>Rozwiąż skibidi quiz i znajdź swoje nowe hobby!</h1>
+class QuizSolver extends Component{
+    state = {
+        questions: null,
+    };
+
+    componentDidMount(){
+        axios.get(this.props.address).then(res => {
+            this.setState({
+                questions: res.data,
+            });
+        })
+        .catch(error => {
+            console.error("Błąd zapytania API:", error);
+        });
+    }
+
+    render(){
+        const questionsList = this.state.questions && this.state.questions.map(question =>{
+            return(
+                <div className='pytanie'>
+                    <p>Pytanie {question.id_pytania}: {question.text_pytania}</p>
+                    <ul>
+                        {question.answers.map(answer => (
+                            <li key={answer.id}>
+                            {answer.id}: {answer.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        });
+
+
+        return(
             <div>
-                
+                {questionsList ? questionsList : <h1>Nie udało się wczytać bazy pytań :(</h1>}
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default QuizSolver;
